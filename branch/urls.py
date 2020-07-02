25441d79ebe249c5f9a1372855908eb29f5cb5b1
nitin1011 <nitinjethwani10@gmail.com>
2020-07-03 03:03:34 +0530

    update url

--- branch/urls.py
+++ branch/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='home'),
-    path('/detail', views.branch_detail, name='branch_detail'),
-    path('/all', views.branch_all, name='branch_all'),
+    path('detail', views.branch_detail, name='branch_detail'),
+    path('all', views.branch_all, name='branch_all'),
 ]
