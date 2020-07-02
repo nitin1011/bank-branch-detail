ef0132ce5ccf55c4a9a8beec53c7f3e5ae1f8a26
nitin1011 <nitinjethwani10@gmail.com>
2020-07-03 02:13:19 +0530

    update urls

--- Test/urls.py
+++ Test/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('branch/', include('branch.urls')),
+    path('', include('branch.urls')),
 ]
