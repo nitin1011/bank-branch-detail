7486aae33d7afa917af9d489c5dbeed6d0f038fc
nitin1011 <nitinjethwani10@gmail.com>
2020-07-03 05:52:25 +0530

    commit

--- Test/settings.py
+++ Test/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'nitin1011',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
